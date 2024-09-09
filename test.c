6eb58b3f1bea330592e92b9a6697a998a881287c
mohamed3moustir <mohamedmoustir99@gmail.com>
2024-09-09 13:04:10 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,17 +1,1 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
 
-int main(){
-
-	char nams[50][100]={"mohamed","ali","kmal","said"};
-	for(int i=0;i<4;i++){
-   if(strcmp(nams[i],"ali")== 0){
-	printf("found in %i",i);
-
-   }
-
-	}
-	
-	return 0;
-}
